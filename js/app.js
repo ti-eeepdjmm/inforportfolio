c4c66ad39ae4c5f79f33fde0b5a1e916d2cb35d6
Henrique Alves <henryworkdev.ti@gmail.com>
2022-06-03 07:45:23 -0300

    ajustando menu mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-export function bemVindo(){
-    console.log('Bem vindo!')
+const btn_menu = document.querySelector(".btn-toggle");
+const menu = document.querySelector(".menu")
+
+
+btn_menu.onclick = function(){
+  if(menu.classList.contains('menu--visivel')){
+    menu.classList.remove('menu--visivel');
+  }else{
+    menu.classList.add('menu--visivel');  
+  }
+  
 }
